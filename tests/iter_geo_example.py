0002d056b4df6e8f73ae38ee3e9a0187a3d45b64
Colm Duffy <colm.p.duffy@gmail.com>
2024-05-14 08:24:58 +0100

    update

---
+++ tests/iter_geo_example.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from crop_lca.models import load_crop_farm_data
+from crop_lca.lca import ClimateChangeTotals
+from crop_lca.geo_crop_lca.catchment_crop_production import CatchmentCropData
+
+
+def main():
+    names_file = "./data/geo_data/catchment_names.csv"
+
+    catchment_names = pd.read_csv(names_file)
+    # Instantiate ClimateChange Totals Class, passing Ireland as the emissions factor country
+    climatechange = ClimateChangeTotals("ireland")
+
+    # Create a dictionary to store results
+    index = 2020
+
+    ef_country = "ireland"
+
+    for name in catchment_names["Catchment"]:
+        crop_emissions_dict = climatechange.create_emissions_dictionary([index])
+
+        # Create some data to generate results
+        data = CatchmentCropData.gen_catchment_crop_production_dataframe(ef_country, name, index)
+        
+        data_frame = pd.DataFrame(data)
+
+        #proportion of fertiliser inputs that is urea
+        urea_proportion =0.2
+        urea_abated_proportion = 0
+        # generate results and store them in the dictionary
+
+        data = load_crop_farm_data(data_frame)
+
+        crop_emissions_dict["crop_residue_direct"][index] += (
+            climatechange.total_residue_per_crop_direct(
+                data[index],
+            )
+        )
+        crop_emissions_dict["N_direct_fertiliser"][index] += (
+            climatechange.total_fertiliser_direct(
+                data[index],
+                urea_proportion,
+                urea_abated_proportion,
+            )
+            
+        )
+        crop_emissions_dict["N_indirect_fertiliser"][index] += (
+            climatechange.total_fertiliser_indirect(
+                data[index],
+                urea_proportion,
+                urea_abated_proportion,
+            )
+        )
+        crop_emissions_dict["soils_N2O"][index] += (
+            crop_emissions_dict["crop_residue_direct"][index]
+            + crop_emissions_dict["N_direct_fertiliser"][index]
+            + crop_emissions_dict["N_indirect_fertiliser"][index]
+        )
+        crop_emissions_dict["soils_CO2"][index] += (
+            climatechange.urea_co2(
+                data[index],
+                urea_proportion,
+                urea_abated_proportion,
+            )
+            
+        )
+
+        print(crop_emissions_dict)
+        print("#"*50)
+
+
+if __name__ == "__main__":
+    main()
